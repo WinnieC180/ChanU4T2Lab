13f02be02cd1986431e1ce07c15e97a5537ee010
Winnie Chan <winniec18@nycstudents.net>
2022-12-31 09:41:24 -0500

    upload

--- src/StringLoops.java
+++ src/StringLoops.java
@@ -1,5 +1,47 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class StringLoops
+{
+    // default constructor; no instance variables
+    public StringLoops() { }
+
+    /* Returns the number of times "character" appears in "searchString"
+       This should be NON-case sensitive!
+
+       Examples:
+       - if character = "a" and searchString = "Apple and banana",
+         this method returns 5 (it finds BOTH "A" and "a")
+       - if character = "A" and searchString = "Apple and banana",
+         this method returns 5 (it finds BOTH "A" and "a")
+       - if character = "!" and searchString = "Hello! Nice day!",
+         this method returns 2
+
+        DO THIS WITH A FOR LOOP!
+      */
+    public int countCharacters(String character, String searchString) {
+        searchString = searchString.toLowerCase();
+        character = character.toLowerCase();
+        int count =0;
+
+        for (int i = 0; i < searchString.length(); i++) {
+            String c = searchString.substring(i, i+1); //i++ does not work in a substring
+
+            if (c.equals(character)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /* Returns the original string reversed
+
+     Examples:
+     - if origString = "hello!" this method returns "!olleh"
+     - if origString = "Apples and bananas" this method returns "sananab dna selppA"
+    */
+    public String reverseString(String origString) {
+        String reversed = "";
+        for (int i = origString.length()-1; i>=0; i--) {
+            reversed += origString.substring(i, i+1);
+        }
+        return reversed;
     }
 }
--- src/StringLoopsRunner.java
+++ src/StringLoopsRunner.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class StringLoopsRunner {
+public class StringLoopsRunner {
+    public static void main (String[] arg) {
+        StringLoops loops = new StringLoops();
+
+        System.out.println(loops.countCharacters("a", "Apple and banana"));
+        System.out.println(loops.countCharacters("A", "Apple and banana"));
+        System.out.println(loops.countCharacters("!", "Hello! Nice day!"));
+        System.out.println(loops.reverseString("hello!"));
+        System.out.println(loops.reverseString("Apples and bananas"));
+    }
 }
